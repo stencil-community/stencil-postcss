40512999f7c86efad30df0f16f767fabf56606f6
Adam Bradley <adambradley25@gmail.com>
2018-02-14 14:46:35 -0600

    update for publish

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # @stencil/postcss
 
-This package is used in order to integrate with postcss and all of it's plugins.
+This package is used in order to integrate with postcss and all of its plugins.
 
 First, npm install within the project:
 
@@ -9,13 +9,16 @@ npm install @stencil/postcss --save-dev
 ```
 
 Next, within the project's `stencil.config.js` file, import the plugin and add
-it to the config's `plugins` config:
+it to the `plugins` config. In the example below we're using the `autoprefixer` postcss plugin, so you'll also have to run:
+
+```
+npm install autoprefixer --save-dev
+```
 
 ```js
 const postcss = require('@stencil/postcss');
-
-// if you want to run autoprefixer
 const autoprefixer = require('autoprefixer');
+
 exports.config = {
   plugins: [
     postcss({
@@ -26,12 +29,12 @@ exports.config = {
 ```
 
 During development, this plugin will use postcss to process any plugins you may
-have passed along
+have passed along.
 
 ## Options
 
 Postcss has an ecosystem of plugins itself (a plugin for a plugin if you will).
-For our example, we're using the autoprefixer plugin, and configuring it's
+For our example, we're using the autoprefixer plugin, and configuring its
 options. Note, you can pass any valid autoprefixer option.
 
 ```js
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as postCss from 'postcss';
 
+
 module.exports = function postcss(
   options: { plugins?: Array<postCss.AcceptedPlugin> } = {}
 ) {
@@ -8,198 +9,46 @@ module.exports = function postcss(
       if (!options.hasOwnProperty('plugins') || options.plugins.length < 1) {
         return null;
       }
+
       if (!context || !usePlugin(id)) {
         return null;
       }
+
       const results: PluginTransformResults = {};
-      const pathParts = id.split('.');
-      pathParts.pop();
-      pathParts.push('css');
-      results.id = pathParts.join('.');
+      results.id = id
+
       if (sourceText.trim() === '') {
         results.code = '';
         return Promise.resolve(results);
       }
-      return new Promise<PluginTransformResults>(resolve => {
-        postCss(options.plugins)
-          .process(sourceText, {
-            from: id
-          })
-          .then(async postCssResults => {
-            postCssResults.warnings().forEach(err => {
-              loadDiagnostic(context, err, id);
-              results.code = `/**  sass error${err.toString()}  **/`;
-            });
+
+      return postCss(options.plugins)
+        .process(sourceText, {
+          from: id
+        })
+        .then(async postCssResults => {
+          const warnings = postCssResults.warnings().map(warn => {
+            return `${warn.type}: ${warn.text}`;
+          });
+
+          if (warnings.length > 0) {
+            results.code = `/**  postcss error${warnings.join(', ')}  **/`;
+
+          } else {
             results.code = postCssResults.css;
             await context.fs.writeFile(results.id, results.code, {inMemoryOnly: true});
-            resolve(results);
-          });
-      });
+          }
+
+          return results;
+        });
     },
-    name: 'postCss'
+    name: 'postcss'
   };
 };
 
 function usePlugin(id: string) {
-  return /(.scss|.sass|.css)$/i.test(id);
+  return /(.css)$/i.test(id);
 }
-
-function loadDiagnostic(context: PluginCtx, sassError: any, filePath: string) {
-  if (!sassError || !context) {
-    return;
-  }
-
-  const d: Diagnostic = {
-    level: 'error',
-    type: 'sass',
-    language: 'scss',
-    header: 'sass error',
-    code: sassError.status && sassError.status.toString(),
-    relFilePath: null,
-    absFilePath: null,
-    messageText: sassError.message,
-    lines: []
-  };
-
-  if (filePath) {
-    d.absFilePath = filePath;
-    d.relFilePath = formatFileName(context.config.rootDir, d.absFilePath);
-    d.header = formatHeader(
-      'sass',
-      d.absFilePath,
-      context.config.rootDir,
-      sassError.line
-    );
-
-    if (sassError.line > -1) {
-      try {
-        const sourceText = context.fs.readFileSync(d.absFilePath);
-        const srcLines = sourceText.split(/(\r?\n)/);
-
-        const errorLine: PrintLine = {
-          lineIndex: sassError.line - 1,
-          lineNumber: sassError.line,
-          text: srcLines[sassError.line - 1],
-          errorCharStart: sassError.column,
-          errorLength: 0
-        };
-
-        for (let i = errorLine.errorCharStart; i >= 0; i--) {
-          if (STOP_CHARS.indexOf(errorLine.text.charAt(i)) > -1) {
-            break;
-          }
-          errorLine.errorCharStart = i;
-        }
-
-        for (
-          let j = errorLine.errorCharStart;
-          j <= errorLine.text.length;
-          j++
-        ) {
-          if (STOP_CHARS.indexOf(errorLine.text.charAt(j)) > -1) {
-            break;
-          }
-          errorLine.errorLength++;
-        }
-
-        if (errorLine.errorLength === 0 && errorLine.errorCharStart > 0) {
-          errorLine.errorLength = 1;
-          errorLine.errorCharStart--;
-        }
-
-        d.lines.push(errorLine);
-
-        if (errorLine.lineIndex > 0) {
-          const previousLine: PrintLine = {
-            lineIndex: errorLine.lineIndex - 1,
-            lineNumber: errorLine.lineNumber - 1,
-            text: srcLines[errorLine.lineIndex - 1],
-            errorCharStart: -1,
-            errorLength: -1
-          };
-
-          d.lines.unshift(previousLine);
-        }
-
-        if (errorLine.lineIndex + 1 < srcLines.length) {
-          const nextLine: PrintLine = {
-            lineIndex: errorLine.lineIndex + 1,
-            lineNumber: errorLine.lineNumber + 1,
-            text: srcLines[errorLine.lineIndex + 1],
-            errorCharStart: -1,
-            errorLength: -1
-          };
-
-          d.lines.push(nextLine);
-        }
-      } catch (e) {
-        console.error(`StyleSassPlugin loadDiagnostic, ${e}`);
-      }
-    }
-  }
-
-  context.diagnostics.push(d);
-}
-
-function formatFileName(rootDir: string, fileName: string) {
-  if (!rootDir || !fileName) return '';
-
-  fileName = fileName.replace(rootDir, '');
-  if (/\/|\\/.test(fileName.charAt(0))) {
-    fileName = fileName.substr(1);
-  }
-  if (fileName.length > 80) {
-    fileName = '...' + fileName.substr(fileName.length - 80);
-  }
-  return fileName;
-}
-
-function formatHeader(
-  type: string,
-  fileName: string,
-  rootDir: string,
-  startLineNumber: number = null,
-  endLineNumber: number = null
-) {
-  let header = `${type}: ${formatFileName(rootDir, fileName)}`;
-
-  if (startLineNumber !== null && startLineNumber > 0) {
-    if (endLineNumber !== null && endLineNumber > startLineNumber) {
-      header += `, lines: ${startLineNumber} - ${endLineNumber}`;
-    } else {
-      header += `, line: ${startLineNumber}`;
-    }
-  }
-
-  return header;
-}
-
-const STOP_CHARS = [
-  '',
-  '\n',
-  '\r',
-  '\t',
-  ' ',
-  ':',
-  ';',
-  ',',
-  '{',
-  '}',
-  '.',
-  '#',
-  '@',
-  '!',
-  '[',
-  ']',
-  '(',
-  ')',
-  '&',
-  '+',
-  '~',
-  '^',
-  '*',
-  '$'
-];
 
 export interface PluginTransformResults {
   code?: string;
@@ -207,30 +56,5 @@ export interface PluginTransformResults {
 }
 
 export interface PluginCtx {
-  config: {
-    rootDir: string;
-  };
   fs: any;
-  diagnostics: Diagnostic[];
-}
-
-export interface Diagnostic {
-  level: 'error' | 'warn' | 'info' | 'log' | 'debug';
-  type: string;
-  header?: string;
-  messageText: string;
-  language?: 'javascript' | 'typescript' | 'scss' | 'css';
-  code?: string;
-  absFilePath?: string;
-  relFilePath?: string;
-  lines?: PrintLine[];
-}
-
-export interface PrintLine {
-  lineIndex: number;
-  lineNumber: number;
-  text?: string;
-  html?: string;
-  errorCharStart: number;
-  errorLength?: number;
 }
