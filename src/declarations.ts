dc22271ee612ad4ad46d743f65f71dab50510f74
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-06-08 16:54:20 +0200

    chore(): update to stencil one

--- src/declarations.ts
+++ src/declarations.ts
@@ -1,45 +1,11 @@
+export * from '@stencil/core/internal';
+
 export interface PluginOptions {
   injectGlobalPaths?: string[];
-  plugins?: Array<any>;
-}
-
-export interface PluginTransformResults {
-  code?: string;
-  id?: string;
+  plugins?: any[];
 }
 
 export interface RendererOptions {
   data: string;
-  plugins: Array<any>;
-}
-
-export interface PluginCtx {
-  config: {
-    rootDir?: string;
-    srcDir?: string;
-  };
-  fs: any;
-  diagnostics: Diagnostic[];
-}
-
-export interface Diagnostic {
-  level: 'error' | 'warn' | 'info' | 'log' | 'debug';
-  type: 'typescript' | 'bundling' | 'build' | 'runtime' | 'hydrate' | 'css';
-  header?: string;
-  language?: string;
-  messageText: string;
-  code?: string;
-  absFilePath?: string;
-  relFilePath?: string;
-  lineNumber?: number;
-  columnNumber?: number;
-  lines?: PrintLine[];
-}
-
-export interface PrintLine {
-  lineIndex: number;
-  lineNumber: number;
-  text?: string;
-  errorCharStart: number;
-  errorLength?: number;
+  plugins: any[];
 }
